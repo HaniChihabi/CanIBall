c155fcdfcf70d3bd4b72d54a2e57bcd0e6dd1619
HaniChihabi <hanichihabih@gmail.com>
2024-01-05 18:22:14 +0100

    got rid off old app and kept new app

--- app/service/apiservice.js
+++
@@ -1,5 +0,0 @@
-export const fetchCityName = async (cityName) =>{
-    // Fetch city data from the API
-    const apiKey = '294249189d29841b5a3b8791204c6411'; 
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`);
-}
